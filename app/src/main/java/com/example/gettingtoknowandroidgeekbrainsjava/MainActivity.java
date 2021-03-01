e66331af9235728af2f049dfb55452a185793b45
artemternovenko <artem.inkaz@gmail.com>
2021-03-01 10:04:37 +0600

    [FIXED] blank lines

--- app/src/main/java/com/example/gettingtoknowandroidgeekbrainsjava/MainActivity.java
+++ app/src/main/java/com/example/gettingtoknowandroidgeekbrainsjava/MainActivity.java
@@ -1,39 +1,28 @@
 package com.example.gettingtoknowandroidgeekbrainsjava;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         Button buttonActivity = findViewById(R.id.button_activity2);
-
-
-       buttonActivity.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               try {
-                   MainActivity2();
-               }catch (Exception e) {
-                   Log.d("TAG","Error Activity");
-
-               }
-           }
-       });
+        buttonActivity.setOnClickListener(v -> {
+            try {
+                MainActivity2();
+            } catch (Exception e) {
+                Log.d("TAG", "Error Activity");
+            }
+        });
     }
 
-    private void MainActivity2(){
+    private void MainActivity2() {
         Intent intent = new Intent(MainActivity.this, MainActivity2.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/gettingtoknowandroidgeekbrainsjava/MainActivity2.java
+++ app/src/main/java/com/example/gettingtoknowandroidgeekbrainsjava/MainActivity2.java
@@ -1,5 +1,6 @@
 package com.example.gettingtoknowandroidgeekbrainsjava;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,55 +8,43 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare_digits);
-
-       EditText numberOneEditText = findViewById(R.id.number_one_edit_text);
-       EditText numberTwoEditText = findViewById(R.id.number_two_edit_text);
-       Button buttonCompare = findViewById(R.id.button_compare);
+        EditText numberOneEditText = findViewById(R.id.number_one_edit_text);
+        EditText numberTwoEditText = findViewById(R.id.number_two_edit_text);
+        Button buttonCompare = findViewById(R.id.button_compare);
         Button buttonActivity = findViewById(R.id.button_activity3);
-       TextView resultText = findViewById(R.id.result_text);
-
-       buttonCompare.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               try {
-                   Integer valueOne = Integer.valueOf(numberOneEditText.getText().toString());
-                   Integer valueTwo = Integer.valueOf(numberTwoEditText.getText().toString());
-
-                   if (valueOne.equals(valueTwo)) {
-                       resultText.setText("Equals");
-                   } else {
-                       resultText.setText(" Not Equals!");
-                   }
-               }catch (NumberFormatException e) {
-                   Log.d("TAG","Incorrect enter");
-                   resultText.setText(" Please enter right number!");
-               }
-           }
-       });
-
-        buttonActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    MainActivity3();
-                }catch (Exception e) {
-                    Log.d("TAG","error Activity");
+        TextView resultText = findViewById(R.id.result_text);
+        buttonCompare.setOnClickListener(v -> {
+            try {
+                Integer valueOne = Integer.valueOf(numberOneEditText.getText().toString());
+                Integer valueTwo = Integer.valueOf(numberTwoEditText.getText().toString());
+                if (valueOne.equals(valueTwo)) {
+                    resultText.setText("Equals");
+                } else {
+                    resultText.setText(" Not Equals!");
                 }
+            } catch (NumberFormatException e) {
+                Log.d("TAG", "Incorrect enter");
+                resultText.setText(" Please enter right number!");
             }
         });
-
+        buttonActivity.setOnClickListener(v -> {
+            try {
+                MainActivity3();
+            } catch (Exception e) {
+                Log.d("TAG", "error Activity");
+            }
+        });
     }
 
-    private void MainActivity3(){
+    private void MainActivity3() {
         Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
         startActivity(intent);
     }
