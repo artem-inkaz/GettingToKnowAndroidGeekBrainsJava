18f3600bb6c8f9afeaf3a3be14e4186dbd101ec5
artemternovenko <artem.inkaz@gmail.com>
2021-03-10 18:16:11 +0600

    [ADDED] CalculatorActivity3.java ThemeActivity.java, SharedPreferences

---
+++ app/src/main/java/com/example/gettingtoknowandroidgeekbrainsjava/lesson4/CalculatorActivity3.java
@@ -0,0 +1,184 @@
+package com.example.gettingtoknowandroidgeekbrainsjava.lesson4;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gettingtoknowandroidgeekbrainsjava.R;
+import com.example.gettingtoknowandroidgeekbrainsjava.lesson3.Calculator;
+import com.google.android.material.textfield.TextInputLayout;
+
+
+public class CalculatorActivity3 extends ThemeActivity {
+
+    private TextView resultField; // текстовое поле для вывода результата
+    private TextInputLayout mTextInputLayout;
+    private EditText numberField;   // поле для ввода числа
+    private final static String KEY_CALCULATOR = "CALCULATOR";
+    private final static String KEY_RESULT_FIELD = "RESULT_FIELD";
+    private final static String KEY_NUMBER_FIELD = "NUMBER_FIELD";
+
+    Calculator calculator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(getAppTheme(R.style.AppThemeLight));
+        setContentView(R.layout.activity_calculator);
+
+        calculator = new Calculator();
+        resultField = findViewById(R.id.textView_result);
+        numberField = findViewById(R.id.edit_text_input);
+
+        initButtonClickListener(R.id.button_0);
+        initButtonClickListener(R.id.button_1);
+        initButtonClickListener(R.id.button_2);
+        initButtonClickListener(R.id.button_3);
+        initButtonClickListener(R.id.button_4);
+        initButtonClickListener(R.id.button_5);
+        initButtonClickListener(R.id.button_6);
+        initButtonClickListener(R.id.button_7);
+        initButtonClickListener(R.id.button_8);
+        initButtonClickListener(R.id.button_9);
+        initButtonClickListener(R.id.button_dot);
+
+        initButtonAddClickListener();
+        initButtonEqualClickListener();
+        initButtonSubClickListener();
+        initButtonDivClickListener();
+        initButtonMultClickListener();
+        initButtonOnClickListener();
+
+        initButtonSetThemeClickListener(R.id.button_light_mode,AppThemeLight);
+        initButtonSetThemeClickListener(R.id.button_night_mode,AppThemeDark);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+//        outState.putSerializable(KEY_CALCULATOR, calculator);
+        outState.putParcelable(KEY_CALCULATOR, calculator);
+        outState.putString(KEY_RESULT_FIELD, String.valueOf(resultField.getText().toString()));
+        outState.putString(KEY_NUMBER_FIELD, String.valueOf(numberField.getText().toString()));
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+//        calculator = (Calculator) savedInstanceState.getSerializable(KEY_CALCULATOR);
+        calculator = (Calculator) savedInstanceState.getParcelable(KEY_CALCULATOR);
+        numberField.setText(savedInstanceState.getString(KEY_NUMBER_FIELD));
+        resultField.setText(savedInstanceState.getString(KEY_RESULT_FIELD));
+
+        initButtonClickListener(R.id.button_0);
+        initButtonClickListener(R.id.button_1);
+        initButtonClickListener(R.id.button_2);
+        initButtonClickListener(R.id.button_3);
+        initButtonClickListener(R.id.button_4);
+        initButtonClickListener(R.id.button_5);
+        initButtonClickListener(R.id.button_6);
+        initButtonClickListener(R.id.button_7);
+        initButtonClickListener(R.id.button_8);
+        initButtonClickListener(R.id.button_9);
+        initButtonClickListener(R.id.button_dot);
+
+        initButtonAddClickListener();
+        initButtonEqualClickListener();
+        initButtonSubClickListener();
+        initButtonDivClickListener();
+        initButtonMultClickListener();
+        initButtonOnClickListener();
+
+    }
+
+    public void initButtonClickListener(int id) {
+        Button btn = findViewById(id);
+        btn.setOnClickListener(v -> {
+            numberField.append(btn.getText());
+            calculator.incrementSubStr((String) btn.getText());
+            if (calculator.getOperand() != null) {
+                resultField.setText(calculator.getSubStr());
+            }
+        });
+    }
+
+    public void initButtonSetThemeClickListener(int id,final int codeStyle) {
+        Button btn = findViewById(id);
+        btn.setOnClickListener(v -> {
+            // сохраним настройки
+            setAppTheme(codeStyle);
+            // пересоздадим активити, чтобы тема применилась
+            recreate();
+        });
+    }
+
+
+    public void initButtonAddClickListener() {
+        Button btnAdd = findViewById(R.id.button_add);
+        btnAdd.setOnClickListener(v -> {
+            numberField.append(btnAdd.getText());
+            calculator.operationAdd(calculator.getSubStr());
+        });
+    }
+
+    public void initButtonOnClickListener() {
+        Button btnClear = findViewById(R.id.button_clear);
+        btnClear.setOnClickListener(v -> {
+            numberField.setText("");
+            calculator.operationOn();
+            resultField.setText("");
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void initButtonSubClickListener() {
+        Button btnSub = findViewById(R.id.button_sub);
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberField.append(btnSub.getText());
+                calculator.operationSub(calculator.getSubStr());
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void initButtonDivClickListener() {
+        Button btnDiv = findViewById(R.id.button_div);
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberField.append(btnDiv.getText());
+                calculator.operationDiv(calculator.getSubStr());
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void initButtonMultClickListener() {
+        Button btnMult = findViewById(R.id.button_multi);
+        btnMult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberField.append(btnMult.getText());
+                calculator.operationMul(calculator.getSubStr());
+            }
+        });
+    }
+
+    private void initButtonEqualClickListener() {
+        Button btnEqual = findViewById(R.id.button_equal);
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultField.setText(calculator.magicCalc().toString());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/gettingtoknowandroidgeekbrainsjava/lesson4/ThemeActivity.java
@@ -0,0 +1,59 @@
+package com.example.gettingtoknowandroidgeekbrainsjava.lesson4;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gettingtoknowandroidgeekbrainsjava.R;
+
+public abstract class ThemeActivity extends AppCompatActivity {
+
+    // Имя настроек
+    private static final String NameSharedPreference = "LOGIN";
+
+    // Имя параметра в настройках
+    private static final String AppTheme = "APP_THEME";
+
+    protected static final int AppThemeLight = 0;
+    protected static final int AppThemeDark = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Устанавливать тему надо только до установки макета активити
+        setTheme(getAppTheme(R.style.AppThemeLight));
+    }
+
+    int getAppTheme(int codeStyle) {
+        return codeStyleToStyleId(getCodeStyle(codeStyle));
+    }
+
+    // Чтение настроек, параметр тема
+    protected int getCodeStyle(int codeStyle){
+        // Работаем через специальный класс сохранения и чтения настроек
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
+        //Прочитать тему, если настройка не найдена - взять по умолчанию
+        return sharedPref.getInt(AppTheme, codeStyle);
+    }
+
+    // Сохранение настроек
+    protected void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference, MODE_PRIVATE);
+        // Настройки сохраняются посредством специального класса editor.
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(AppTheme, codeStyle);
+        editor.apply();
+    }
+
+    private int codeStyleToStyleId(int codeStyle){
+        switch(codeStyle){
+            case AppThemeLight:
+                return R.style.AppThemeLight;
+            case AppThemeDark:
+                return R.style.AppThemeDark;
+            default:
+                return R.style.AppTheme;
+        }
+    }
+}
